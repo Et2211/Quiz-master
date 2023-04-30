255ee1caf2107e7de67a5ee4008a7cc8c24a5cc8
Etienne <e.sharkey@live.com>
2023-05-01 00:13:52 +0100

    Rename app to root (react router best practice)

---
+++ src/Root.tsx
@@ -0,0 +1,23 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Questions from './Questions'
+import './index.css'
+import './style.scss'
+import NoPage from "./components/NoPage/NoPage";
+import StartPage from "./components/StartPage/StartPage";
+
+export default function Root() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/questions" element={<Questions />} />
+        <Route path="/completed" element={<Questions />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(<Root />);
