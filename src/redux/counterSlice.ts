662bafb595ff1128c4ef714b30a7761f8961501a
Etienne <e.sharkey@live.com>
2023-05-01 02:35:17 +0100

    Create counterSlice, with actions to increment, decrement or change score by a set amount

---
+++ src/redux/counterSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from './store'
+
+// Define a type for the slice state
+interface CounterState {
+    score: number
+}
+
+// Define the initial state using that type
+const initialState: CounterState = {
+    score: 0,
+}
+
+export const counterSlice = createSlice({
+  name: 'score',
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.score += 1
+    },
+    decrement: (state) => {
+      state.score -= 1
+    },
+    // Use the PayloadAction type to declare the contents of `action.payload`
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.score += action.payload
+    },
+  },
+})
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions
+
+// Other code such as selectors can use the imported `RootState` type
+export const selectCount = (state: RootState) => state.score
+
+export default counterSlice.reducer
